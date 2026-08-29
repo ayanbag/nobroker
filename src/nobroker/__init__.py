ca5f60b2b51efd1146fcc819161a75f9f1a7c6c0
Ayan Bag <ayanbag9474@gmail.com>
2026-08-29 17:25:41 +0530

    ci: macos test removed

--- src/nobroker/__init__.py
+++ src/nobroker/__init__.py
@@ -32,7 +32,7 @@ from .logfile import RecoveryReport
 from .queue import CompactionResult, Queue
 from .worker import Worker, WorkerStats
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "BackoffPolicy",
